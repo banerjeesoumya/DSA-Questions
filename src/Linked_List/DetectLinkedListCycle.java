3463c42403efcd79e21bee730083075d9f27392e
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-05-22 14:06:48 +0530

    Detecting Linked List Cycle

---
+++ src/Linked_List/DetectLinkedListCycle.java
@@ -0,0 +1,36 @@
+package Linked_List;
+
+public class DetectLinkedListCycle {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        // Create a loop
+        fifth.next = third;
+
+        if(hasCycle(head)) {
+            System.out.println("There is a cycle");
+        } else {
+            System.out.println("There is not a cycle");
+        }
+    }
+
+    public static boolean hasCycle(Node head) {
+        Node fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
