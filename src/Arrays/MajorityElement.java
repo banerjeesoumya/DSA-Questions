203b50857f1363491043e50f46d602fefa6667e9
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-11 11:53:31 +0530

    Updated

--- src/Arrays/MajorityElement.java
+++ src/Arrays/MajorityElement.java
@@ -4,14 +4,29 @@ import java.util.HashMap;
 
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] arr = {3,2,3};
+        int[] arr = {2,2,1,1,1,2,2};
         int max = majorityElement(arr);
         System.out.println(max);
     }
 
     public static int majorityElement(int[] nums) {
 
-//        Better Solution
+//        Optimal Approach (Time Complexity - O(n) Space Complexity - O(1))
+        int count = 0;
+        int el = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                count ++;
+                el = nums[i];
+            } else if (nums[i] == el) {
+                count ++;
+            } else {
+                count --;
+            }
+        }
+        return el;
+
+//        Better Solution (Time Complexity - O(N) + O(NlogN), Space Complexity - O(1))
 //        HashMap<Integer, Integer> map = new HashMap<>();
 //        for (int i = 0; i < nums.length; i++) {
 //            int val = map.getOrDefault(nums[i], 0);
