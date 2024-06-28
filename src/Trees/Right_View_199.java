7ba29b131fb4b331c86da14af532830a782096a3
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-28 11:50:35 +0530

    Updated

--- src/Trees/Right_View_199.java
+++ src/Trees/Right_View_199.java
@@ -1,2 +1,56 @@
-package Trees;public class Right_View_199 {
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Right_View_199 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.left.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        Right_View_199 obj = new Right_View_199();
+        List<Integer> res  = obj.rightView(root);
+        for (int el : res) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
+    }
+
+    public List<Integer> rightView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node != null) {
+                    if (i == size - 1) {
+                        res.add(node.val);
+                    }
+                }
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+
+
 }
+
