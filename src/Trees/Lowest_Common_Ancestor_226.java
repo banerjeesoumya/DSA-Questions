51b8b5e724be69c72fdc58b3b63fccd3ae54cfe9
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-06-30 12:12:41 +0530

    Lowest Common Ancestor

---
+++ src/Trees/Lowest_Common_Ancestor_226.java
@@ -0,0 +1,39 @@
+package Trees;
+
+public class Lowest_Common_Ancestor_226 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        Lowest_Common_Ancestor_226 lca = new Lowest_Common_Ancestor_226();
+        TreeNode ans = lca.lowestCommonAncestor(root, root.left, root.right.right);
+        System.out.println(ans.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return right != null ? right : left;
+    }
+
+}
