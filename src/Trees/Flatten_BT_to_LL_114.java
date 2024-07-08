da2617f92bbf94f52080bd5d94fea3bc8c8a56c0
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-07-08 16:07:25 +0530

    Flattened binary tree to linked list

--- src/Trees/Flatten_BT_to_LL_114.java
+++ src/Trees/Flatten_BT_to_LL_114.java
@@ -1,2 +1,31 @@
-package Trees;public class Flatten_BT_to_LL_114 {
+package Trees;
+
+public class Flatten_BT_to_LL_114 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(6);
+
+        Flatten_BT_to_LL_114 obj = new Flatten_BT_to_LL_114();
+        flatten(root);
+    }
+
+    public static void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode temp = curr.left;
+                while (temp.right != null) {
+                    temp = temp.right;
+                }
+                temp.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
 }
