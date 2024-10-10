3b5db0e6c6b3f07a2d216461275543c229a11244
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2024-10-10 23:58:23 +0530

    No:of provinces

---
+++ src/Graphs/No_of_provinces.java
@@ -0,0 +1,28 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class No_of_provinces {
+    public static void main(String[] args) {
+        Graph graph = new Graph(4);
+        graph.addEdge(1, 0);
+//        graph.addEdge(2, 1);
+        graph.addEdge(3, 0);
+        graph.addEdge(2, 0);
+
+        No_of_provinces ob = new No_of_provinces();
+        System.out.println(ob.findNoOfProvinces(graph.adj, graph));
+    }
+
+    public int findNoOfProvinces(ArrayList<ArrayList<Integer>> adj, Graph graph) {
+        boolean[] vis = new boolean[adj.size()];
+        int count = 0;
+        for (int i = 0; i < adj.size(); i++) {
+            if (!vis[i]) {
+                count ++;
+                graph.dfs_recur1(i, adj, vis);
+            }
+        }
+        return count;
+    }
+}
