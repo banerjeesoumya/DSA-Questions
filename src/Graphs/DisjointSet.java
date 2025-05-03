f47c35067c24617dd0c4cbae71893a3ca741d820
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-05-03 15:08:54 +0530

    Added the union by size functionality

--- src/Graphs/DisjointSet.java
+++ src/Graphs/DisjointSet.java
@@ -6,26 +6,37 @@ import java.util.List;
 public class DisjointSet {
     List<Integer> rank = new ArrayList<Integer>();
     List<Integer> parent = new ArrayList<Integer>();
+    List<Integer> size = new ArrayList<Integer>();
     public DisjointSet(int n) {
         for (int i = 0; i <= n; i ++) {
             rank.add(0);
             parent.add(i);
+            size.add(1);
         }
     }
 
     public static void main(String[] args) {
         DisjointSet ds = new DisjointSet(7);
-        ds.unionByRank(1, 2);
-        ds.unionByRank(2, 3);
-        ds.unionByRank(4, 5);
-        ds.unionByRank(6, 7);
-        ds.unionByRank(5, 6);
+//        Find Union By Rank
+//        ds.unionByRank(1, 2);
+//        ds.unionByRank(2, 3);
+//        ds.unionByRank(4, 5);
+//        ds.unionByRank(6, 7);
+//        ds.unionByRank(5, 6);
+//        Find Union By Size
+        ds.unionBySize(1, 2);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(4, 5);
+        ds.unionBySize(6, 7);
+        ds.unionBySize(5, 6);
+
         if (ds.findParent(3) == ds.findParent(7)) {
             System.out.println("Same");
         } else {
             System.out.println("Not Same");
         }
-        ds.unionByRank(3, 7);
+//        ds.unionByRank(3, 7);
+        ds.unionBySize(3, 7);
         if (ds.findParent(3) == ds.findParent(7)) {
             System.out.println("Same");
         } else {
@@ -56,6 +67,21 @@ public class DisjointSet {
             parent.set(ulp_v, ulp_u);
             int currRankU = rank.get(ulp_u);
             rank.set(ulp_u, currRankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (ulp_u == ulp_v) {
+            return;
+        }
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, (size.get(ulp_u) + size.get(ulp_v)));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, (size.get(ulp_u) + size.get(ulp_v)));
         }
     }
 }
