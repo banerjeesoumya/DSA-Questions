bf9da37a783e57d75f27f22a245a897ef01dcee0
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-08-13 20:56:46 +0530

    Updated

--- src/DP/Longest_Common_Subsequence/Tabulation.java
+++ src/DP/Longest_Common_Subsequence/Tabulation.java
@@ -1,8 +1,6 @@
 package DP.Longest_Common_Subsequence;
 
-import java.util.Arrays;
-
-public class Memoization {
+public class Tabulation {
     public static void main(String[] args) {
         String text1a = "abcde";
         String text2a = "ace";
@@ -23,24 +21,21 @@ public class Memoization {
         int m = text1.length();
         int n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int i = 0; i <= m; i ++ ) {
+            dp[i][0] = 0;
         }
-        return dp(m, n, text1, text2, dp);
-    }
-    public static int dp(int i, int j, String text1, String text2, int[][] dp) {
-        if (i == 0 || j == 0) {
-            return 0;
+        for (int j = 0; j <= n; j ++ ) {
+            dp[0][j] = 0;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-        if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-            dp[i][j] =  1 + dp(i - 1, j - 1, text1, text2, dp);
-            return dp[i][j];
-        } else {
-            dp[i][j] =  0 + Math.max(dp(i - 1, j, text1, text2, dp), dp(i, j - 1, text1, text2, dp));
-            return dp[i][j];
-        }
+        return dp[m][n];
     }
 }
